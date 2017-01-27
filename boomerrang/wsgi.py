494b576b2570f30359acd2671831db20e5584dcc
Andrea Mitchell <andrealeemitchell@gmail.com>
2017-01-27 14:25:50 -0800

    add Django path in wsgi.py

--- boomerrang/wsgi.py
+++ boomerrang/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys 
 
 from django.core.wsgi import get_wsgi_application
+
+
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boomerrang.settings")
 
