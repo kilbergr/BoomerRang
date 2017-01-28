4f76b3eaa04467bb1b6470daa888872cebc8b176
Rebecca Kilberg <rekilberg@gmail.com>
2017-01-27 17:12:56 -0800

    updated requirements to include twilio, add psql db

--- boomerrang/settings.py
+++ boomerrang/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'boomerrang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boomerrang_db',
+        'USER': 'admin',
+        'PASSWORD': 'truss',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
