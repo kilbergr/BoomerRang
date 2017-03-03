afaa2e80ce4b341e16cebc7242a35fa1e653e5dd
Andrea Mitchell <andrealeemitchell@gmail.com>
2017-03-03 11:18:02 -0800

    fix things

--- boomerrang/boomerrang_app/models.py
+++ boomerrang/boomerrang_app/models.py
@@ -4,17 +4,18 @@ from django.core.validators import RegexValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-
 class Org(models.Model):
     username = models.CharField(max_length=254)
     password = models.CharField(max_length=254)
-    
+
+
 class CallRequest(models.Model):
     source_num = PhoneNumberField()
     target_num = PhoneNumberField()
     call_completed = models.NullBooleanField()
     time_scheduled = models.DateTimeField(auto_now=False)
     org = models.ForeignKey(Org, related_name="call_requests")
+
 
 class Call(models.Model):
     call_time = models.DateTimeField(auto_now=False)
