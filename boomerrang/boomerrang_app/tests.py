7c329f79bdca4de506167de9eeb834b208e9cbfa
Rebecca Kilberg <rekilberg@gmail.com>
2017-03-31 16:19:53 -0700

    adding test descriptions

--- boomerrang/boomerrang_app/tests.py
+++ boomerrang/boomerrang_app/tests.py
@@ -9,31 +9,38 @@ from . import view_helpers
 class ModelTests(TestCase):
 
 	def test_callrequest_has_org(self):
-		# Create two org objects
+		# Given: Two org objects
 		org = Org(username='boblah', password='blah')
 		org2 = Org(username='org2', password='pw2')
-		# Create a call_req with one of them
+
+		# And: A call_req associated with one of them
 		call_req = CallRequest(
 			source_num='+15107571675', target_num='+15107571675', org=org)
 
-		# Expected org should be on call_request obj
+		# When: Call_req obj is examined
+		# Then: Expected org should be on call_request obj
 		self.assertEqual(call_req.org, org)
 		self.assertNotEqual(call_req.org, org2)
 
 	def test_call_must_have_callrequest_to_save(self):
-		# Create a call object without a request
+		# Given: A call object without a request
 		call_time = timezone.now()
 		call = Call(call_time=call_time)
 
-		# Raise exception when attempting to save call obj
+		# When: Call obj is saved
 		with self.assertRaises(Exception):
+			# Then: Raise exception when attempting to save call obj
 			call.save()
 
 
 class ViewTests(TestCase):
 
     def test_index_response(self):
+    	# Given: Views
+    	# When: Home view is visited
         response = self.client.get('/')
+
+        # Then: Expect form in response, and status_code of 200
         self.assertEqual(response.status_code, 200)
         self.assertTrue('form' in response.context)
 
@@ -51,7 +58,11 @@ class ViewTests(TestCase):
 class ViewHelpersTests(TestCase):
 
     def test_load_twilio_config(self):
+    	# Given: View_helpers
+    	# When: Twilio configs are loaded
     	test_num, test_sid, test_token = view_helpers.load_twilio_config()
+
+    	# Then: configs conform to expectations
     	self.assertEqual(len(test_token), 32)
     	self.assertEqual(len(test_sid), 34)
     	self.assertIn('+1', test_num)
