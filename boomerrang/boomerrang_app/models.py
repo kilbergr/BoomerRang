bd0b815aecf8a58b98308a40f61dc2de50d1f092
Andrea Mitchell <andrealeemitchell@gmail.com>
2017-04-20 16:10:28 -0700

    added changes

--- boomerrang/boomerrang_app/models.py
+++ boomerrang/boomerrang_app/models.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 
 from phonenumber_field.modelfields import PhoneNumberField
 
+log = logger.getLogger("boom_logger")
 
 class Org(models.Model):
     username = models.CharField(max_length=254)
@@ -14,11 +15,25 @@ class CallRequest(models.Model):
     target_num = PhoneNumberField()
     call_completed = models.NullBooleanField()
     time_scheduled = models.DateTimeField(auto_now=False)
-    org = models.ForeignKey(Org, models.SET_NULL, blank=True, null=True, related_name="call_requests")
+    org = models.ForeignKey(Org, on_delete=models.SET_NULL, null=True, related_name="call_requests")
+
+    def save(self, *args, **kwargs):
+        if not self.org:
+
+            return # must have an org to be entered into db
+        else:
+            super(CallRequest, self).save(*args, **kwargs)
 
 
 class Call(models.Model):
     call_time = models.DateTimeField(auto_now=False)
     success = models.NullBooleanField()
     duration = models.DateTimeField(auto_now=False, null=True)
-    call_request = models.ForeignKey(CallRequest,  models.SET_NULL, blank=True, null=True, related_name="calls")
+    call_request = models.ForeignKey(CallRequest, on_delete=models.SET_NULL, null=True, related_name="calls")
+
+    def save(self, *args, **kwargs):
+        if not self.call_request:
+            return # must have an org to be entered into db
+        else:
+            super(Call, self).save(*args, **kwargs)
+
