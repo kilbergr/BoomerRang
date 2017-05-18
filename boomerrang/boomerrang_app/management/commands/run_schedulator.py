dc575ee0fc652ac1a0b6a3189b95902430e49187
Rebecca Kilberg <rekilberg@gmail.com>
2017-05-18 15:52:09 -0700

    added logging to be able to better track progress

--- boomerrang/boomerrang_app/management/commands/run_schedulator.py
+++ boomerrang/boomerrang_app/management/commands/run_schedulator.py
@@ -14,17 +14,27 @@ log = logging.getLogger('boom_logger')
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        # Determine how many calls have been attempted per call_request
         annotation = {'calls_count': Count('calls')}
+        # Filter for call_requests scheduled before now with fewer than 3 calls attempted
         filters = {'time_scheduled__lt': timezone.now(), 'calls_count__lt': 3}
+        # Exclude call_requests that have call_completed=True
         exclusions = {'call_completed': True}
         requests = CallRequest.objects.annotate(
             **annotation).filter(**filters).exclude(**exclusions)
 
+        # Log if there are no call requests for the period in question
+        if len(requests)==0:
+            info_msg = 'There are no calls to make for this period.'
+            log.info(info_msg)
+
         for request in requests:
-            # If any related call objects were successful, skip request
+            # If any related calls were successful or are still in progress, skip
             unfailed_calls = [(call.success or call.success is None)
                               for call in request.calls.all()]
             if any(unfailed_calls):
+                info_msg = 'This call request has calls that were successful or are currently in progress.'
+                log.info(info_msg)
                 continue
 
             call = Call.objects.create(
@@ -33,3 +43,5 @@ class Command(BaseCommand):
 
             make_call(request)
             call.save()
+            info_msg = 'A call has been made via the schedulator.'
+            log.info(info_msg)
