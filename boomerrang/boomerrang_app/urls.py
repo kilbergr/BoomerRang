67e2e8cba880f75f2362494dea59029cdd92e7e4
Rebecca Kilberg <rekilberg@gmail.com>
2017-05-24 13:28:46 -0700

    adding accidentally removed P from url

--- boomerrang/boomerrang_app/urls.py
+++ boomerrang/boomerrang_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^outbound\/(?P<target_num>\+1[0-9]{10})\/$',
     	views.outbound, name='outbound'),
-    url(r'^call-status\/(?P<call_req_id>\d+)\/(?<call_id>\d+)\/$',
+    url(r'^call-status\/(?P<call_req_id>\d+)\/(?P<call_id>\d+)\/$',
     	views.call_status, name='call_status'),
 ]
