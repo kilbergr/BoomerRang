4dc368a672d23da5cff0e25c9f221c2dbddfb44e
Andrea Mitchell <andrealeemitchell@gmail.com>
2017-08-11 11:52:14 -0400

    Fix schedulator logging  (#47)
    reviewers: @kilbergr

--- boomerrang/boomerrang_app/management/commands/run_schedulator.py
+++ boomerrang/boomerrang_app/management/commands/run_schedulator.py
@@ -14,6 +14,21 @@ log = logging.getLogger('boom_logger')
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        """
+        Creates calls for all Call Request objects that have fewer than 3
+        associated call objects and none of these calls have completed
+        successfully.
+
+        If there is an associated call object, a call has been attempted. The call is
+        considered a 'success' if the source caller has picked up the phone and is
+        considered unsuccessful (call object success value is set to False), if
+        it went to voice message or the call did not go through.
+
+        If a call is still in progress while the schedulator runs again, its success
+        field hasn't been set to True (set to "None"), and the
+        schedulator will not initiate a call.
+
+        """
         # Determine how many calls have been attempted per call_request
         annotation = {'calls_count': Count('calls')}
         # Filter for call_requests scheduled before now with <3 calls attempted
@@ -24,19 +39,36 @@ class Command(BaseCommand):
                    'source_num__blacklisted': False}
         # Exclude call_requests that have call_completed=True
         exclusions = {'call_completed': True}
+
         requests = CallRequest.objects.annotate(
             **annotation).filter(**filters).exclude(**exclusions)
 
-        # Log if there are no call requests for the period in question
-        if len(requests) == 0:
-            log.info('There are no calls to make for this period.')
-
+        has_made_call = False
         for request in requests:
-            # If any calls were successful or are still in progress, skip
+            # TODO: @andie - change 'success' attribute on call object to 'status'
+            # in order have more useful values. Success should only be True/False.
+            # call.status should be set to something like "in progress" when a
+            # call is initiated, so we can explicitly check for it below.
+
+            # If any related calls were successful or are still in progress, skip
             unfailed_calls = [(call.success or call.success is None)
-                              for call in request.calls.all()]
+                             for call in request.calls.all()]
+
+
             if any(unfailed_calls):
                 continue
 
             launch_call_process(request)
-            log.info('A call has been made via the schedulator.')
+
+            log.info('A call has been made from {} to {} at the scheduled time of {}.'.format(
+                request.source_num.number,
+                request.target_num,
+                request.time_scheduled
+            ))
+
+            has_made_call = True
+
+        # Log if there are no call requests for the period in question
+        if not has_made_call:
+            info_msg = 'There are no calls to make for this period.'
+            log.info(info_msg)
